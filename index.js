892ca58fa1b96e3b255ade798eab1fd21ab62de4
Gabriel <gabriel.castro@castrom.net>
2025-07-04 08:00:18 -0300

    Adicionando templates na engine com o HANDLEBARS e conectando informações do formulário com o bodyparser

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars');
+const {engine} = require("express-handlebars");
+const bodyParser = require("body-parser"); 
+const Sequelize = require('sequelize');
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/html/index.html");
-    //o __dirname serve para "encurtar o nome do diretório" + a rota: html/index.html
+
+// Config
+    //template engine
+    app.engine("handlebars", engine({defaultLayout: "main"}));
+    app.set("view engine", "handlebars");
+
+    //Body parser
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+
+    //conexao com o banco de dados MySQL
+    const sequelize = new Sequelize("estudo", "root", "root123", {
+    host: "localhost",
+    dialect: "mysql"
 
 });
 
-app.get("/sobre", function(req, res){
-    res.sendFile(__dirname + "/html/sobre.html");
-});
 
-app.get("/blog", function(req, res){
-    res.send("Bem vindo ao meu blog");
-});
+//rotas
 
-app.get("/ola/:nome/:cargo", function(req, res){
-    res.send("Olá "+req.params.nome);
-    //o res.send só pode ser usado uma vez na requisição
-});
+    app.get("/cad", function(req, res){
+        res.render("formulario")
+    });
 
+    app.post("/add", function(req, res){
+        req.body.conteudo
+        res.send("Texto: "+ req.body.titulo + " Conteudo: " + req.body.conteudo)
+    });
 
 app.listen(8081, function(){
     console.log("Servidor rodando!!!!")
 });
+
